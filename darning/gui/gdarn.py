66effa14cce20f45ff404128b60013f8c900fbe2
Peter Williams <peter_ono@users.sourceforge.net>
2012-06-22 15:32:35 +1000

    gui: change groupings in tool bar to a more logical arrangement
    Signed-off-by: Peter Williams <peter_ono@users.sourceforge.net>

--- darning/gui/gdarn.py
+++ darning/gui/gdarn.py
@@ -50,15 +50,15 @@ class Darning(gtk.Window, dialogue.BusyIndicator, actions.AGandUIManager):
         <toolbar name="gdarn_patches_toolbar">
            <separator/>
             <toolitem action="file_list_add_new"/>
-            <toolitem name="Fold" action="patch_list_fold_external_patch"/>
+           <separator/>
             <toolitem name="Refresh" action="patch_list_refresh_top_patch"/>
-            <separator/>
             <toolitem name="Push" action="patch_list_push"/>
             <toolitem name="Pop" action="patch_list_pop"/>
-             <separator/>
+           <separator/>
             <toolitem name="New" action="patch_list_new_patch"/>
             <toolitem name="Import" action="patch_list_import_patch"/>
-             <separator/>
+            <toolitem name="Fold" action="patch_list_fold_external_patch"/>
+           <separator/>
             <toolitem name="Select" action="patch_list_select_guards"/>
         </toolbar>
     </ui>
