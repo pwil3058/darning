a540a3cfc214910903fa671d7368874c45f0e403
Peter Williams <pwil3058@gmail.com>
2013-05-21 14:21:27 +1000

    terminal: add copy/paste functionality via popup menu
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- darning/gui/terminal.py
+++ darning/gui/terminal.py
@@ -1,14 +1,14 @@
 ### Copyright (C) 2010 Peter Williams <peter_ono@users.sourceforge.net>
-
+###
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
 ### the Free Software Foundation; version 2 of the License only.
-
+###
 ### This program is distributed in the hope that it will be useful,
 ### but WITHOUT ANY WARRANTY; without even the implied warranty of
 ### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ### GNU General Public License for more details.
-
+###
 ### You should have received a copy of the GNU General Public License
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -29,6 +29,7 @@ try:
             self._vte.set_size_request(200, 50)
             self._vte.set_scrollback_lines(-1)
             self._vte.show()
+            self._vte.connect('button_press_event', self._button_press_cb)
             scrbar = gtk.VScrollbar(self._vte.get_adjustment())
             scrbar.show()
             self.pack_start(self._vte)
@@ -37,5 +38,21 @@ try:
             self._pid = self._vte.fork_command()
         def set_cwd(self, path):
             self._vte.feed_child("cd %s\n" % path)
+        def _button_press_cb(self, widget, event):
+            if event.type == gtk.gdk.BUTTON_PRESS:
+                if event.button == 3:
+                    menu = gtk.Menu()
+                    copy_item = gtk.MenuItem(label=_('Copy'))
+                    copy_item.set_sensitive(widget.get_has_selection())
+                    copy_item.connect_object('activate', vte.Terminal.copy_clipboard, widget)
+                    paste_item = gtk.MenuItem(label=_('Paste'))
+                    paste_item.set_sensitive(gtk.Clipboard().wait_is_text_available())
+                    paste_item.connect_object('activate', vte.Terminal.paste_clipboard, widget)
+                    menu.append(copy_item)
+                    menu.append(paste_item)
+                    menu.show_all()
+                    menu.popup(None, None, None, event.button, event.time)
+                    return True
+            return False
 except ImportError:
     AVAILABLE = False
