db178d25c6bf9de709a68f7535db62c0e31d01d3
Peter Williams <pwil3058@gmail.com>
2016-10-28 11:17:11 +1000

    mixins: remove dead code and Python 3ize class declarations
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- darning/mixins.py
+++ darning/mixins.py
@@ -1,4 +1,4 @@
-### Copyright (C) 2015 Peter Williams <peter_ono@users.sourceforge.net>
+###  Copyright (C) 2016 Peter Williams <pwil3058@gmail.com>
 ###
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
@@ -13,24 +13,8 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-class ExtensiblePickleObject(object):
-    '''A base class for pickleable objects that can cope with modifications'''
-    RENAMES = dict()
-    NEW_FIELDS = dict()
-    def __setstate__(self, state):
-        self.__dict__ = state
-        for old_field in self.RENAMES:
-            if old_field in self.__dict__:
-                self.__dict__[self.RENAMES[old_field]] = self.__dict__.pop(old_field)
-    def __getstate__(self):
-        return self.__dict__
-    def __getattr__(self, attr):
-        if attr in self.NEW_FIELDS:
-            return self.NEW_FIELDS[attr]
-        raise AttributeError(attr)
-
 # CITE: http://code.activestate.com/recipes/578433-mixin-for-pickling-objects-with-__slots__/
-class PedanticSlotPickleMixin(object):
+class PedanticSlotPickleMixin:
     def __getstate__(self):
         return {slot : getattr(self, slot) for slot in self.__slots__}
     def __setstate__(self, state):
@@ -42,7 +26,7 @@ class PedanticSlotPickleMixin(object):
                 return False
         return True
 
-class LenientSlotPickleMixin(object):
+class LenientSlotPickleMixin:
     def __getstate__(self):
         return {slot : getattr(self, slot) for slot in self.__slots__ if hasattr(self, slot)}
     def __setstate__(self, state):
@@ -54,7 +38,7 @@ class LenientSlotPickleMixin(object):
                 return False
         return True
 
-class WrapperMixin(object):
+class WrapperMixin:
     WRAPPED_ATTRIBUTES = list()
     WRAPPED_OBJECT_NAME = "_WRAPPED_OBJECT"
     def __getattr__(self, attr_name):
